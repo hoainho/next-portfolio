0a60d18d51aa2b812af787d293503c02147d89d0
hoainho <nhoxtvt@gmail.com>
2024-07-07 21:07:38 +0700

    feat: add loader for glasses model

--- components/loader/LoaderWithProgressPercent.tsx
+++ components/loader/LoaderWithProgressPercent.tsx
@@ -1,26 +1,15 @@
 import { Html, useProgress } from "@react-three/drei";
 
 const CanvasLoader = () => {
-  const { progress, item } = useProgress();
-  let loader;
-  switch (true) {
-    case item.includes("/3d/"):
-    case item.includes("/images/"):
-    case item.includes("/certifications/"):
-      loader = null;
-      break;
-    default:
-      loader = (
-        <Html as="div" center className="flex-center flex-col">
-          <span className="canvas-loader"></span>
-          <p className="text-[18px] text-[#40c9ff] font-extrabold mt-[40px]">
-            {progress.toFixed(2)}%
-          </p>
-        </Html>
-      );
-      break;
-  }
-  return loader;
+  const { progress } = useProgress();
+  return (
+    <Html as="div" center className="flex-center flex-col">
+      <span className="canvas-loader"></span>
+      <p className="text-[18px] text-[#40c9ff] font-extrabold mt-[40px]">
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
 };
 
 export default CanvasLoader;
