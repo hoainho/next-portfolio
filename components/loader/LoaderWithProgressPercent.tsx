8919f22b4d36c752eaad4b2f7e121820cbfd9a8c
hoainho <nhoxtvt@gmail.com>
2024-07-19 16:58:58 +0700

    fix: re-calc for value in progress

--- components/loader/LoaderWithProgressPercent.tsx
+++ components/loader/LoaderWithProgressPercent.tsx
@@ -2,16 +2,16 @@ import { Html, useProgress } from "@react-three/drei";
 import { Progress } from "@/components/ui/progress";
 
 const CanvasLoader = () => {
-  const data = useProgress();
+  const { loaded, total} = useProgress();
   return (
     <Html as="div" center className="flex-center flex-col">
       <p className="animate-bounce text-[22px] text-[#40c9ff] font-extrabold mt-[40px]">
-        {data.loaded} / {data.total}
+        {loaded} / {total}
       </p>
       <div className="flex-center gap-[10px]">
-        <Progress value={data.progress} className="w-[200px] sm:w-[280px]" />
+        <Progress value={loaded / total * 100} className="w-[200px] sm:w-[280px]" />
         <p className="text-[18px] text-[#40c9ff] font-black">
-          {data.progress.toFixed(2)}%
+          {(loaded / total * 100).toFixed(2)}%
         </p>
       </div>
     </Html>
