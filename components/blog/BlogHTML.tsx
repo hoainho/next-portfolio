8fe42c1cf975f884483dc8dff063377d48a90365
hoainho <nhoxtvt@gmail.com>
2024-10-22 23:06:32 +0700

    fix: space on top content

--- components/blog/BlogHTML.tsx
+++ components/blog/BlogHTML.tsx
@@ -14,7 +14,7 @@ const BlogHTML = ({ post }: BlogHTMLProps) => {
     hljs.highlightAll();
   }, [post.content.rendered]);
   return (
-    <div className='w-full flex gap-x-5 relative overflow-hidden rounded-2xl shadow-lg p-8 mt-4 bg-gradient-to-br from-blue-300 via-purple-200 to-red-200'>
+    <div className='w-full flex gap-x-5 relative overflow-hidden rounded-2xl shadow-lg p-8 bg-gradient-to-br from-blue-300 via-purple-200 to-red-200'>
       <div className="absolute inset-0 bg-gradient-to-br from-transparent to-black/10 opacity-30"></div>
       <div className='flex flex-col gap-y-5' dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
     </div>
