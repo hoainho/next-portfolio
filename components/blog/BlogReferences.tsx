7d2b8c4b989ed3513ab08a7190d9c9ed22e0194b
hoainho <nhoxtvt@gmail.com>
2024-10-28 11:29:18 +0700

    fix: remove v1

--- components/blog/BlogReferences.tsx
+++ components/blog/BlogReferences.tsx
@@ -12,7 +12,7 @@ const BlogReferences = ({ posts }: BlogReferenceProps) => {
       <h3 className='text-xl font-bold text-primary mb-4'>References posts</h3>
       {posts.map((post: BlogType) => (
         <div key={post.id} className='flex flex-col items-start justify-start gap-2 border-t border-blog-divider py-2'>
-          <Link href={`/blog/v2/${post.id}`} className='text-gradient-purple-coral font-bold text-md mt-5'>{post.title.rendered}</Link>
+          <Link href={`/blog/${post.id}`} className='text-gradient-purple-coral font-bold text-md mt-5'>{post.title.rendered}</Link>
           <div className='text-fg-muted text-sm' dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
           <p className='text-primary font-mono font-semibold text-sm'>{post.yoast_head_json.author}</p>
         </div>
