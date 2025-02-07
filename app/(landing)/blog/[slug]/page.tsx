80a9396d698496db9f9fe9e4796d68d22ef11ffa
thuonglh <thuonglh@dtp-education.com>
2025-02-07 14:47:47 +0700

    update format

--- app/(landing)/blog/[slug]/page.tsx
+++ app/(landing)/blog/[slug]/page.tsx
@@ -20,9 +20,9 @@ import BlogRelated from "@/components/blog/BlogRelated";
 import client from "@/lib/apolloClient";
 import BlogSubscribers from "@/components/blog/BlogSubscribers";
 import {
-	GET_POSTS_BY_CATEGORY_AND_AUTHOR_QUERY,
-	POST_DETAIL_QUERY,
-	POST_DETAIL_SEO_QUERY,
+  GET_POSTS_BY_CATEGORY_AND_AUTHOR_QUERY,
+  POST_DETAIL_QUERY,
+  POST_DETAIL_SEO_QUERY,
 } from "@/graphql/queries/post.query";
 import ImageLoader from "@/components/loader/ImageLoader";
 import NotFoundPage from "@/app/not-found";
@@ -31,285 +31,285 @@ import { INCREMENT_POST_VIEWS_MUTATION } from "@/graphql/mutations/post.mutation
 import ButtonCopyURL from "@/components/blog/ButtonCopyURL";
 
 type BlogDetailProps = {
-	params: {
-		blogId: string;
-		slug: string;
-	};
-	searchParams: {
-		slug: string;
-	};
+  params: {
+    blogId: string;
+    slug: string;
+  };
+  searchParams: {
+    slug: string;
+  };
 };
 
 export async function generateMetadata({
-	params,
+  params,
 }: BlogDetailProps): Promise<Metadata> {
-	try {
-		const fetchPost = await client.query({
-			query: POST_DETAIL_SEO_QUERY,
-			variables: { slug: params.slug },
-		});
+  try {
+    const fetchPost = await client.query({
+      query: POST_DETAIL_SEO_QUERY,
+      variables: { slug: params.slug },
+    });
 
-		const post: PostSEO = fetchPost?.data?.post;
+    const post: PostSEO = fetchPost?.data?.post;
 
-		if (!post) {
-			return {
-				title: "Post Not Found",
-				description: "Post not found",
-				openGraph: {
-					title: "Post Not Found",
-					description: "Post not found",
-					url: "/blog",
-					images: [],
-				},
-				twitter: {
-					title: "Post Not Found",
-					description: "Post not found",
-					images: [],
-				},
-			};
-		}
+    if (!post) {
+      return {
+        title: "Post Not Found",
+        description: "Post not found",
+        openGraph: {
+          title: "Post Not Found",
+          description: "Post not found",
+          url: "/blog",
+          images: [],
+        },
+        twitter: {
+          title: "Post Not Found",
+          description: "Post not found",
+          images: [],
+        },
+      };
+    }
 
-		return {
-			title: `${post.title || "The latest blog posts"} | Nick's Blog`,
-			description: post.seo.metaDesc,
-			robots: {
-				index: true,
-				follow: true,
-				googleBot: {
-					index: true,
-					follow: true,
-				},
-			},
-			openGraph: {
-				title: post.title,
-				description: post.seo.metaDesc,
-				url: post.seo.canonical,
-				images: [
-					{
-						url: decodeURIComponent(post.seo.opengraphImage.sourceUrl),
-						width: 1280,
-						height: 720,
-						alt: post.seo.opengraphImage.altText,
-						type: "image/png",
-					},
-				],
-				locale: "en_US",
-			},
-			twitter: {
-				card: "summary_large_image",
-				title: post.title,
-				description: post.seo.metaDesc,
-				images: [
-					{
-						url: decodeURIComponent(post.seo.opengraphImage.sourceUrl),
-						width: 1280,
-						height: 720,
-						alt: post.seo.opengraphImage.altText,
-						type: "image/png",
-					},
-				],
-				site: post.seo.opengraphSiteName || "Hoài Nhớ",
-			},
-		};
-	} catch (error) {
-		console.log("error", error);
-		return {
-			title: "Post Not Found",
-			description: "Post not found",
-			openGraph: {
-				title: "Post Not Found",
-				description: "Post not found",
-				url: "/blog",
-				images: [],
-			},
-			twitter: {
-				title: "Post Not Found",
-				description: "Post not found",
-				images: [],
-			},
-		};
-	}
+    return {
+      title: `${post.title || "The latest blog posts"} | Nick's Blog`,
+      description: post.seo.metaDesc,
+      robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+          index: true,
+          follow: true,
+        },
+      },
+      openGraph: {
+        title: post.title,
+        description: post.seo.metaDesc,
+        url: post.seo.canonical,
+        images: [
+          {
+            url: decodeURIComponent(post.seo.opengraphImage.sourceUrl),
+            width: 1280,
+            height: 720,
+            alt: post.seo.opengraphImage.altText,
+            type: "image/png",
+          },
+        ],
+        locale: "en_US",
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: post.title,
+        description: post.seo.metaDesc,
+        images: [
+          {
+            url: decodeURIComponent(post.seo.opengraphImage.sourceUrl),
+            width: 1280,
+            height: 720,
+            alt: post.seo.opengraphImage.altText,
+            type: "image/png",
+          },
+        ],
+        site: post.seo.opengraphSiteName || "Hoài Nhớ",
+      },
+    };
+  } catch (error) {
+    console.log("error", error);
+    return {
+      title: "Post Not Found",
+      description: "Post not found",
+      openGraph: {
+        title: "Post Not Found",
+        description: "Post not found",
+        url: "/blog",
+        images: [],
+      },
+      twitter: {
+        title: "Post Not Found",
+        description: "Post not found",
+        images: [],
+      },
+    };
+  }
 }
 
 const BlogDetail = async ({ params }: BlogDetailProps) => {
-	const fetchPost = await client.query({
-		query: POST_DETAIL_QUERY,
-		variables: { slug: params.slug },
-	});
+  const fetchPost = await client.query({
+    query: POST_DETAIL_QUERY,
+    variables: { slug: params.slug },
+  });
 
-	const post: PostItem = fetchPost?.data?.post;
+  const post: PostItem = fetchPost?.data?.post;
 
-	const responseIncrement = await client.mutate({
-		mutation: INCREMENT_POST_VIEWS_MUTATION,
-		variables: { postId: post?.postId },
-		context: {
-			fetchOptions: {
-				cache: "no-store",
-			},
-		},
-	});
+  const responseIncrement = await client.mutate({
+    mutation: INCREMENT_POST_VIEWS_MUTATION,
+    variables: { postId: post?.postId },
+    context: {
+      fetchOptions: {
+        cache: "no-store",
+      },
+    },
+  });
 
-	const counterView = responseIncrement?.data?.incrementPostViews?.postViews?.total;
+  const counterView = responseIncrement?.data?.incrementPostViews?.postViews?.total;
 
-	if (!post) {
-		return <NotFoundPage />;
-	}
+  if (!post) {
+    return <NotFoundPage />;
+  }
 
-	const readingTime = readingDuration(post.content, {
-		wordsPerMinute: 250,
-		emoji: false,
-	});
+  const readingTime = readingDuration(post.content, {
+    wordsPerMinute: 250,
+    emoji: false,
+  });
 
-	const getPostsByCategory = post.categories.nodes?.map((p: PostCategory) => {
-		const postsByCategoryID = client.query({
-			query: GET_POSTS_BY_CATEGORY_AND_AUTHOR_QUERY,
-			variables: { category: p.slug, author: 3, first: 3 },
-		});
-		return postsByCategoryID;
-	});
+  const getPostsByCategory = post.categories.nodes?.map((p: PostCategory) => {
+    const postsByCategoryID = client.query({
+      query: GET_POSTS_BY_CATEGORY_AND_AUTHOR_QUERY,
+      variables: { category: p.slug, author: 3, first: 3 },
+    });
+    return postsByCategoryID;
+  });
 
-	const postRefFilter = await Promise.all(getPostsByCategory);
-	const referencePosts = Object.values(
-		postRefFilter
-			.flatMap((p) => p?.data?.posts?.nodes)
-			.reduce((acc: PostItem[], post) => {
-				const exists = acc.some((p) => p?.postId === post?.postId);
-				if (!exists && post?.postId !== params?.slug) {
-					acc.push(post);
-				}
-				return acc;
-			}, []),
-	)
-		?.filter((p) => p.postId !== post.postId)
-		?.slice(0, 6);
+  const postRefFilter = await Promise.all(getPostsByCategory);
+  const referencePosts = Object.values(
+    postRefFilter
+      .flatMap((p) => p?.data?.posts?.nodes)
+      .reduce((acc: PostItem[], post) => {
+        const exists = acc.some((p) => p?.postId === post?.postId);
+        if (!exists && post?.postId !== params?.slug) {
+          acc.push(post);
+        }
+        return acc;
+      }, []),
+  )
+    ?.filter((p) => p.postId !== post.postId)
+    ?.slice(0, 6);
 
-	let breadcrumbItems = [
-		{ label: "Home", href: "/" },
-		{ label: "Blog", href: "/blog" },
-	];
+  let breadcrumbItems = [
+    { label: "Home", href: "/" },
+    { label: "Blog", href: "/blog" },
+  ];
 
-	breadcrumbItems = breadcrumbItems.concat({
-		label: post.title,
-		href: `/blog/${post.slug}`,
-	});
+  breadcrumbItems = breadcrumbItems.concat({
+    label: post.title,
+    href: `/blog/${post.slug}`,
+  });
 
-	return (
-		<div className="blog">
-			<div className="blog-title relative bg-bg-default">
-				<div className="max-container-blog pt-10 !pb-0">
-					<Breadcrumb items={breadcrumbItems} />
-					<h1 className="font-bold text-5xl mb-4 text-fg-default">
-						{post.title}
-					</h1>
-					<div
-						className="text-[#ADBAA7] text-base mb-10"
-						dangerouslySetInnerHTML={{ __html: post.excerpt }}
-					/>
-					<div className="relative min-h-[180px] xs:min-h-[250px] sm:min-h-[400px] md:min-h-[500px] lg:min-h-[550px] xl:min-h-[600px] h-full w-full z-10">
-						<ImageLoader
-							width={1200}
-							height={480}
-							src={decodeURIComponent(post.featuredImage.node.sourceUrl)}
-							alt={post.featuredImage.node.altText ?? post.title}
-							className="aspect-[4/2.4] rounded-md h-fit w-full absolute top-0 left-0 right-0 z-1"
-						/>
-					</div>
-					<div className="bg-white h-24 w-full absolute bottom-0 left-0 right-0"></div>
-				</div>
-			</div>
-			<div className="bg-white pt-2 md:pt-5 xl:pt-10">
-				<div className="max-container-blog bg-white px-0">
-					<div className="flex justify-between items-center py-5 border-b-2 border-[#545df0] mt-4 mb-8 text-sm text-gray-600 dark:text-gray-300">
-						<div className="flex flex-col gap-3 !font-mono">
-							<div className="flex items-center gap-1">
-								<span className="text-primary font-bold">
-									{post.author.node.name}
-								</span>
-								<LuDot className="inline-block text-base" />
-								<Link
-									href={`/blog/author/${post.author.node.slug}`}
-									className="text-fg-default font-normal"
-								>
-									@{post.author.node.slug}
-								</Link>
-							</div>
-							<div className="flex items-start flex-wrap gap-1 flex-col sm:flex-row sm:items-center ">
-								<div className="flex items-center gap-1">
-									<HiCalendar className="inline-block text-base mr-2" />
-									<span className="text-fg-muted tracking-wide font-thin">
-										{format(
-											new Date(post.modified ?? post.date),
-											"MMMM dd, yyyy",
-										)}
-									</span>
-								</div>
-								<span className="hidden sm:block px-5">|</span>
-								<div className="flex items-center gap-1">
-									<HiOutlineClock className="inline-block text-base mr-2" />
-									<p className="text-fg-muted tracking-wide font-thin">
-										{readingTime}
-									</p>
-								</div>
-								<span className="hidden sm:block px-5">|</span>
-								<BlogCounterView view={counterView} />
-							</div>
-						</div>
-						<div className="flex justify-center items-center gap-3 flex-col sm:flex-row">
-							<h6 className="text-primary">Share:</h6>
-							<div className="flex gap-3 justify-center items-center">
-								<Link
-									target="_blank"
-									href={`https://www.reddit.com/submit?url=${process.env.NEXT_PUBLIC_PORTFOLIO_URL}/blog/${post.slug}`}
-									className="flex items-center p-2 border rounded-full "
-								>
-									<IoLogoReddit className="inline-block text-base" />
-								</Link>
-								<Link
-									target="_blank"
-									href={`https://twitter.com/intent/tweet?url=${process.env.NEXT_PUBLIC_PORTFOLIO_URL}/blog/${post.slug}`}
-									className="flex items-center p-2 border rounded-full"
-								>
-									<FaXTwitter className="inline-block text-base" />
-								</Link>
-								<Link
-									target="_blank"
-									href={`https://www.linkedin.com/sharing/share-offsite/?url=${process.env.NEXT_PUBLIC_PORTFOLIO_URL}/blog/${post.slug}`}
-									className="flex items-center p-2 border rounded-full gap-1"
-								>
-									<LuLinkedin className="inline-block text-base" />
-								</Link>
-								<ButtonCopyURL slug={post.slug} />
-							</div>
-						</div>
-					</div>
-					<div className="flex gap-10">
-						<div className="xl:max-w-[calc(100%-224px)] flex flex-col gap-y-5 w-full">
-							<BlogHTML content={post.content} />
-							<BlogBottomCategories tags={post.tags.nodes?.map((t) => t.name)} />
-							<BlogAuthor author={post.author.node} />
-						</div>
-						<div className="hidden xl:flex flex-col gap-5 max-w-56">
-							<div className="w-full flex flex-wrap gap-1">
-								{post.tags.nodes?.map((tag: TagItem) => (
-									<TagDetail key={tag.id}>{tag.name}</TagDetail>
-								))}
-							</div>
-							<GenerateTableOfContent
-								referencePosts={referencePosts?.slice(0, 3)}
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
-			{referencePosts.length > 0 && (
-				<div className="bg-white flex justify-center items-center gap-3">
-					<BlogRelated posts={referencePosts} />
-				</div>
-			)}
-			<BlogSubscribers />
-			<ScrollToTop />
-		</div>
-	);
+  return (
+    <div className="blog">
+      <div className="blog-title relative bg-bg-default">
+        <div className="max-container-blog pt-10 !pb-0">
+          <Breadcrumb items={breadcrumbItems} />
+          <h1 className="font-bold text-5xl mb-4 text-fg-default">
+            {post.title}
+          </h1>
+          <div
+            className="text-[#ADBAA7] text-base mb-10"
+            dangerouslySetInnerHTML={{ __html: post.excerpt }}
+          />
+          <div className="relative min-h-[180px] xs:min-h-[250px] sm:min-h-[400px] md:min-h-[500px] lg:min-h-[550px] xl:min-h-[600px] h-full w-full z-10">
+            <ImageLoader
+              width={1200}
+              height={480}
+              src={decodeURIComponent(post.featuredImage.node.sourceUrl)}
+              alt={post.featuredImage.node.altText ?? post.title}
+              className="aspect-[4/2.4] rounded-md h-fit w-full absolute top-0 left-0 right-0 z-1"
+            />
+          </div>
+          <div className="bg-white h-24 w-full absolute bottom-0 left-0 right-0"></div>
+        </div>
+      </div>
+      <div className="bg-white pt-2 md:pt-5 xl:pt-10">
+        <div className="max-container-blog bg-white px-0">
+          <div className="flex justify-between items-center py-5 border-b-2 border-[#545df0] mt-4 mb-8 text-sm text-gray-600 dark:text-gray-300">
+            <div className="flex flex-col gap-3 !font-mono">
+              <div className="flex items-center gap-1">
+                <span className="text-primary font-bold">
+                  {post.author.node.name}
+                </span>
+                <LuDot className="inline-block text-base" />
+                <Link
+                  href={`/blog/author/${post.author.node.slug}`}
+                  className="text-fg-default font-normal"
+                >
+                  @{post.author.node.slug}
+                </Link>
+              </div>
+              <div className="flex items-start flex-wrap gap-1 flex-col sm:flex-row sm:items-center ">
+                <div className="flex items-center gap-1">
+                  <HiCalendar className="inline-block text-base mr-2" />
+                  <span className="text-fg-muted tracking-wide font-thin">
+                    {format(
+                      new Date(post.modified ?? post.date),
+                      "MMMM dd, yyyy",
+                    )}
+                  </span>
+                </div>
+                <span className="hidden sm:block px-5">|</span>
+                <div className="flex items-center gap-1">
+                  <HiOutlineClock className="inline-block text-base mr-2" />
+                  <p className="text-fg-muted tracking-wide font-thin">
+                    {readingTime}
+                  </p>
+                </div>
+                <span className="hidden sm:block px-5">|</span>
+                <BlogCounterView view={counterView} />
+              </div>
+            </div>
+            <div className="flex justify-center items-center gap-3 flex-col sm:flex-row">
+              <h6 className="text-primary">Share:</h6>
+              <div className="flex gap-3 justify-center items-center">
+                <Link
+                  target="_blank"
+                  href={`https://www.reddit.com/submit?url=${process.env.NEXT_PUBLIC_PORTFOLIO_URL}/blog/${post.slug}`}
+                  className="flex items-center p-2 border rounded-full "
+                >
+                  <IoLogoReddit className="inline-block text-base" />
+                </Link>
+                <Link
+                  target="_blank"
+                  href={`https://twitter.com/intent/tweet?url=${process.env.NEXT_PUBLIC_PORTFOLIO_URL}/blog/${post.slug}`}
+                  className="flex items-center p-2 border rounded-full"
+                >
+                  <FaXTwitter className="inline-block text-base" />
+                </Link>
+                <Link
+                  target="_blank"
+                  href={`https://www.linkedin.com/sharing/share-offsite/?url=${process.env.NEXT_PUBLIC_PORTFOLIO_URL}/blog/${post.slug}`}
+                  className="flex items-center p-2 border rounded-full gap-1"
+                >
+                  <LuLinkedin className="inline-block text-base" />
+                </Link>
+                <ButtonCopyURL slug={post.slug} />
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-10">
+            <div className="xl:max-w-[calc(100%-224px)] flex flex-col gap-y-5 w-full">
+              <BlogHTML content={post.content} />
+              <BlogBottomCategories tags={post.tags.nodes?.map((t) => t.name)} />
+              <BlogAuthor author={post.author.node} />
+            </div>
+            <div className="hidden xl:flex flex-col gap-5 max-w-56">
+              <div className="w-full flex flex-wrap gap-1">
+                {post.tags.nodes?.map((tag: TagItem) => (
+                  <TagDetail key={tag.id}>{tag.name}</TagDetail>
+                ))}
+              </div>
+              <GenerateTableOfContent
+                referencePosts={referencePosts?.slice(0, 3)}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      {referencePosts.length > 0 && (
+        <div className="bg-white flex justify-center items-center gap-3">
+          <BlogRelated posts={referencePosts} />
+        </div>
+      )}
+      <BlogSubscribers />
+      <ScrollToTop />
+    </div>
+  );
 };
 
 export default BlogDetail;
--- components/blog/ButtonCopyURL.tsx
+++ components/blog/ButtonCopyURL.tsx
@@ -5,32 +5,32 @@ import Button from "../buttons/Button"
 import { FaLink } from "react-icons/fa";
 
 type ButtonCopyProps = {
-	slug: string,
+  slug: string,
 }
 
 const ButtonCopyURL = ({ slug }: ButtonCopyProps) => {
-	const [isShowCopy, setIsShowCopy] = useState(false)
+  const [isShowCopy, setIsShowCopy] = useState(false)
 
-	const handelCopyLink = async () => {
-		const url = `${process.env.NEXT_PUBLIC_PORTFOLIO_URL}/blog/${slug}`
-		await navigator.clipboard.writeText(url);
-		setIsShowCopy(true)
+  const handelCopyLink = async () => {
+    const url = `${process.env.NEXT_PUBLIC_PORTFOLIO_URL}/blog/${slug}`
+    await navigator.clipboard.writeText(url);
+    setIsShowCopy(true)
 
-		setTimeout(() => {
-			setIsShowCopy(false)
-		}, 2000)
-	}
+    setTimeout(() => {
+      setIsShowCopy(false)
+    }, 2000)
+  }
 
-	return (
-		<div className="relative">
-			{isShowCopy && (<span className="absolute bottom-9 -right-2.5 border z-10 p-2 rounded-xl bg-[#233b51] text-white">Copied!</span>)}
-			<Button
-				className="flex items-center border rounded-full gap-1 !px-2 !text-[#007bff]"
-				onClick={() => handelCopyLink()}
-			>
-				<FaLink className="inline-block text-base" />
-			</Button>
-		</div>
-	)
+  return (
+    <div className="relative">
+      {isShowCopy && (<span className="absolute bottom-9 -right-2.5 border z-10 p-2 rounded-xl bg-[#233b51] text-white">Copied!</span>)}
+      <Button
+        className="flex items-center border rounded-full gap-1 !px-2 !text-[#007bff]"
+        onClick={() => handelCopyLink()}
+      >
+        <FaLink className="inline-block text-base" />
+      </Button>
+    </div>
+  )
 }
 export default ButtonCopyURL;
