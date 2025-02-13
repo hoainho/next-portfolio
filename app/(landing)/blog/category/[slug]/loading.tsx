521a3d9f5d3fc03d25c205dc3a89d0be5d30fcb9
thuonglh <thuonglh@dtp-education.com>
2025-02-13 11:24:19 +0700

    create Skeleton container

--- app/(landing)/blog/category/[slug]/loading.tsx
+++ app/(landing)/blog/category/[slug]/loading.tsx
@@ -1,4 +1,4 @@
-import clsx from "clsx";
+import { CardLoading, PlatformLoading } from "@/components/loader/SkeletonContainer";
 
 export default function Loading() {
   return (
@@ -22,16 +22,7 @@ export default function Loading() {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8 mt-4">
               {[...Array(9)].map((_, i) => (
                 <div className="col-span-1 pt-0 md:pt-5">
-                  <div className=" h-[448px] group w-full flex flex-col gap-x-5 gap-3 cursor-pointer py-2 md:py-5 [&:not(:last-child)]:border-b lg:first:pt-0 first:pt-10 border-tertiary" key={i}>
-                    <div className="h-48 bg-[#2d333b] rounded animate-pulse py-3"></div>
-                    <div className="h-6 bg-[#2d333b] rounded animate-pulse max-w-[300px]"></div>
-                    <div className="h-6 bg-[#2d333b] rounded animate-pulse max-w-[300px]"></div>
-                    <div className="h-3 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="h-3 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="h-3 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="h-3 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="h-5 bg-[#2d333b] rounded animate-pulse max-w-[150px]"></div>
-                  </div>
+                  <CardLoading cardKey={i} />
                 </div>
               ))}
             </div>
@@ -44,29 +35,7 @@ export default function Loading() {
           </div>
           <div className="w-3/5 h-6 mt-2 bg-[#2d333b] rounded animate-pulse">
           </div>
-          <div className="w-full flex justify-center gap-5 mt-10 flex-row flex-wrap">
-            {[...Array(4)].map((_, index) => (
-              <div
-                key={index}
-                className={clsx(
-                  "flex flex-col w-full max-w-[260px] items-start justify-between gap-y-10",
-                  "bg-black text-white gap-y-3 px-5 py-14 rounded-sm",
-                )}
-              >
-                <div className="flex flex-col items-start justify-start gap-y-1">
-                  <div className="flex items-center justify-center w-10 h-10 rounded-full bg-[#2d333b] animate-pulse text-active mb-3">
-                  </div>
-                  <div className="h-5 w-44 bg-[#2d333b] animate-pulse rounded"></div>
-                  <div className="h-5 w-44 bg-[#2d333b] animate-pulse rounded"></div>
-                </div>
-                <div
-                  className="flex items-center gap-x-1 text-active mt-8"
-                >
-                  <div className="h-5 w-44 bg-[#2d333b] animate-pulse rounded"></div>
-                </div>
-              </div>
-            ))}
-          </div>
+          <PlatformLoading />
         </div>
       </div>
     </div>
--- app/(landing)/blog/loading.tsx
+++ app/(landing)/blog/loading.tsx
@@ -1,4 +1,4 @@
-import clsx from "clsx";
+import { FeaturedLoading, CardLoading, PlatformLoading } from "@/components/loader/SkeletonContainer";
 
 export default function Loading() {
   return (
@@ -7,31 +7,10 @@ export default function Loading() {
         <div className="fade-in-start max-container-centre py-2 px-5 lg:py-10">
           <div className="relative blog-hero flex flex-col">
             <div className="flex flex-col lg:flex-row w-full gap-x-10 gap-4">
-              <div className="md:w-1/2 w-full flex flex-col gap-4">
-                <div className="w-full h-[336px] bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="w-14 h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="h-8 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="h-8 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="h-3 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="h-3 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="w-1/3 h-5 bg-[#2d333b] rounded animate-pulse"></div>
-              </div>
+              <FeaturedLoading />
               <div className="flex flex-col w-full lg:w-1/2 gap-7">
                 {[...Array(3)].map((_, i) => (
-                  <div className="md:flex flex flex-col gap-3" key={i}>
-                    <div className="w-1/3 h-28 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-full flex flex-col gap-4">
-                      <div className="w-10 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-10/12 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-10/12 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-1/2 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                    </div>
-                  </div>
+                  <CardLoading cardKey={i} flex />
                 ))}
               </div>
             </div>
@@ -44,106 +23,19 @@ export default function Loading() {
       <div className="min-h-screen bg-white text-black">
         <div className="fade-in-start max-container-centre py-14 px-5 lg:py-10">
           <div className="flex gap-10 flex-col lg:flex-row">
-            <div className="flex flex-col w-full lg:w-1/2 gap-10">
-              <div className="h-10 bg-[#2d333b] rounded animate-pulse"></div>
-              {[...Array(3)].map((_, i) => (
-                <div className="md:flex flex flex-col gap-3" key={i}>
-                  <div className="w-1/3 h-28 bg-[#2d333b] rounded animate-pulse"></div>
-                  <div className="w-full flex flex-col gap-4">
-                    <div className="w-10 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-10/12 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-10/12 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-1/2 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="flex flex-col w-full lg:w-1/2 gap-10">
-              <div className="h-10 bg-[#2d333b] rounded animate-pulse"></div>
-              {[...Array(3)].map((_, i) => (
-                <div className="md:flex flex flex-col gap-3" key={i}>
-                  <div className="w-1/3 h-28 bg-[#2d333b] rounded animate-pulse"></div>
-                  <div className="w-full flex flex-col gap-4">
-                    <div className="w-10 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-10/12 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-10/12 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-1/2 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                  </div>
-                </div>
-              ))}
-            </div>
+            {[...Array(2)].map((_, i) => (
+              <div className="flex flex-col w-full lg:w-1/2 gap-10" key={i}>
+                <div className="h-10 bg-[#2d333b] rounded animate-pulse"></div>
+                {[...Array(3)].map((_, i) => (
+                  <CardLoading cardKey={i} flex />
+                ))}
+              </div>
+            ))}
           </div>
         </div>
       </div>
-      <div className="bg-dark text-white min-h-screen overflow-hidden">
-
-        <div className="fade-in-start max-container-centre py-2 px-5 lg:py-10">
-          <div className="relative blog-hero flex flex-col">
-            <div className="h-10 bg-[#2d333b] rounded animate-pulse mb-6"></div>
-            <div className="flex flex-col lg:flex-row w-full gap-x-10 gap-4">
-              <div className="md:w-1/2 w-full flex flex-col gap-4">
-                <div className="w-full h-[336px] bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="w-14 h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="h-8 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="h-8 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="h-3 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="h-3 bg-[#2d333b] rounded animate-pulse"></div>
-                <div className="w-1/3 h-5 bg-[#2d333b] rounded animate-pulse"></div>
-              </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-7 w-full lg:w-1/2">
-                {[...Array(4)].map((_, i) => (
-                  <div key={i} className="flex flex-col gap-3 w-full">
-                    <div className="w-full h-40 bg-[#2d333b] rounded animate-pulse"></div>
-                    <div className="w-full flex flex-col gap-4">
-                      <div className="w-10 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-10/12 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-10/12 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-full h-2 bg-[#2d333b] rounded animate-pulse"></div>
-                      <div className="w-1/2 h-4 bg-[#2d333b] rounded animate-pulse"></div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-
-            </div>
-            <div className="w-full flex justify-center h-12  mb-6 mt-8">
-              <div className="w-1/2 bg-[#2d333b] rounded animate-puls h-full"></div>
-            </div>
-
-            <div className="w-full flex justify-center gap-5 mt-10 flex-row flex-wrap">
-              {[...Array(4)].map((_, index) => (
-                <div
-                  key={index}
-                  className={clsx(
-                    "flex flex-col w-full max-w-[260px] items-start justify-between gap-y-10",
-                    "bg-black text-white gap-y-3 px-5 py-14 rounded-sm",
-                  )}
-                >
-                  <div className="flex flex-col items-start justify-start gap-y-1">
-                    <div className="flex items-center justify-center w-10 h-10 rounded-full bg-[#2d333b] animate-pulse text-active mb-3">
-                    </div>
-                    <div className="h-5 w-44 bg-[#2d333b] animate-pulse rounded"></div>
-                    <div className="h-5 w-44 bg-[#2d333b] animate-pulse rounded"></div>
-                  </div>
-                  <div
-                    className="flex items-center gap-x-1 text-active mt-8"
-                  >
-                    <div className="h-5 w-44 bg-[#2d333b] animate-pulse rounded"></div>
-                  </div>
-                </div>
-              ))}
-            </div>
-
-          </div>
-        </div>
+      <div className="bg-dark py-5 px-5">
+        <PlatformLoading />
       </div>
     </div>
   );
